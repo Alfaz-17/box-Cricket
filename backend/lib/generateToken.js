ba5ca575810cc560cf5eb02d754f9cc14a5d1fc5
Alfaz Bilakhiya <alfazbilakhiya17@gmail.com>
2025-07-06 12:57:44 +0530

    chnges

--- backend/lib/generateToken.js
+++ backend/lib/generateToken.js
@@ -8,11 +8,7 @@ export const generateToken = (userId, res) => {
     expiresIn: "7d",
   });
 
-res.cookie("token", token, {
-  httpOnly: true,
-
-  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-});
+res.cookie("token", token,);
 
   return token;
 };
