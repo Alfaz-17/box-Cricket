91363d3096ce9c4597dbca15b79e2a9746d8e00d
Alfaz Bilakhiya <alfazbilakhiya17@gmail.com>
2025-12-08 14:03:24 +0530

    fix bug

--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -1,12 +1,10 @@
 import express from 'express'
 import { addReview, getReviews } from '../controllers/reviewController.js'
 import { protectedRoute } from '../middleware/auth.js'
-import { markAllAsRead } from '../controllers/notificationController.js'
 
 const router = express.Router()
 
 router.post('/create/:id', protectedRoute, addReview) // List all cricket boxes
 router.get('/:id', protectedRoute, getReviews) // Box details by ID
-router.get('/isRead', protectedRoute, markAllAsRead)
 
 export default router
