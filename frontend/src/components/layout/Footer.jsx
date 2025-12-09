c2ccce8134cd4f455741022c0811874e1a55bb20
Alfaz Bilakhiya <alfazbilakhiya17@gmail.com>
2025-12-09 20:20:57 +0530

    .

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -128,7 +128,7 @@ export default function Footer() {
               </li>
               <li className="flex items-start text-muted-foreground group">
                 <MapPin size={16} className="mr-2 mt-0.5 text-primary group-hover:scale-110 transition-transform" />
-                <span>kesarbai masjid road ,malivalokhancho ,navapara Bhavngar-364001 Gujrat, India</span>
+                <span>navapara Bhavngar-364001 Gujrat, India</span>
               </li>
             </ul>
 
